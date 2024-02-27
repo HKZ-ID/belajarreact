a4f6338f0cfa6f2fa7e37ac32fd6d1efc82502f7
HKZ-ID <aldiraffaw@gmail.com>
2024-02-27 09:23:20 +0700

    first deploy

---
+++ App.js
@@ -0,0 +1,128 @@
+import { StatusBar } from 'expo-status-bar';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>TEST</Text>
+      <Text>😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂</Text>
+      <Text>😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂</Text>
+      <Text>😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂</Text>
+      <StatusBar style="auto" />
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ff0',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
